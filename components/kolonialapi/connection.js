74d9db0800be026f376938c8cf481eda8f2dcb17
henriktorget <henriktorget@gmail.com>
2017-12-09 10:45:44 +0100

    Remade API requesd without request module

---
+++ components/kolonialapi/connection.js
@@ -0,0 +1,13 @@
+var https = require('https');
+var config = require('../../config');
+
+
+/*
+    This is supposed to a dynamic connection to the
+    kolonial.no api. Sould return a connection or
+    just the json file? 
+*/
+
+module.exports = {
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,71 @@
-var express = require('express');
-var router = express.Router();
-var Request = require('request');
-var list;
-var search = "kake";
-var config = require('../config.js');
+const express = require('express');
+const router = express.Router();
+const https = require('https');
+const config = require('../config');
+const connection = require('../components/kolonialapi/connection');
 
-Request.get({
-  "headers": {  "content-type": "application/json",
-                "user-agent": config.secretusername(),
-                "x-client-token": config.secrettoken()},
- "url": "https://kolonial.no/api/v1/search/?q=" + search
-}, (error, response, body) => {
-    if(error) {
-     return console.log(error);
-    }
-
-
-    //console.log(JSON.parse(body));
-   list = JSON.parse(body);
-
-   console.log(list.products[2].name);
-   console.log(list.products[2].images[0].thumbnail.url);
-
-
-});
+let list;
+let search = "kake";
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', {
     title: 'K-Planleggeren',
     search: search,
     data: list });
 });
+
+
+
+
+//Parameters for API search.
+
+let options = {
+  host: 'kolonial.no',
+  port: 443,
+  path: '/api/v1/search/?q=' + search,
+  method: 'GET',
+  headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      "User-Agent": config.secretusername,
+      "X-Client-Token": config.secrettoken
+    }
+};
+
+
+/*
+
+  API request to kolonial.no
+  This should be a module
+
+*/
+
+const req = https.request(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  let chunks = [];
+
+// Datachunks sent back is incrementally pushed into an array.
+  res.on('data', (d) => {
+    chunks.push(d);
+  });
+// Piece together chunks in array and parse.
+  res.on('end', () => {
+    let data = Buffer.concat(chunks);
+    list = JSON.parse(data);
+  });
+});
+
+req.on('error', (e) => {
+  console.error(e);
+});
+
+req.end();
+
+// API request to kolonial.no ends here.
 
 module.exports = router;
