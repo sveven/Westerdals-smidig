c25085f777f497e14ea7d1c46cb38be9f5e1774e
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-12-12 15:58:23 +0100

    Tested to make dynamic, again.

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,30 +6,70 @@ let config = require('../config');
 let list = "";
 let search = "";
 
-
 /* GET recipes page. */
 router.get('/', function(req, res, next) {
     res.render('recipes', {
         title: 'K-Planleggeren',
         search: search,
         data: list});
-    
-    
+
+
 });
 
 router.post('/', function(req, res){
     search = req.body.formsearch;
 
     console.log("Formsearch: " + search);
-    if(search !== "") {
-        onSearch();
-    }
 
-    res.render('recipes', {
-        title: 'K-Planleggeren',
-        search: search,
-        data: list});
+    options = {
+        host: 'kolonial.no',
+        port: 443,
+        path: '/api/v1/search/recipes/?q=' + search,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            "User-Agent": config.secretusername,
+            "X-Client-Token": config.secrettoken
+        }
+    };
+
+
+
+
+
+    /***
+     * Start of testing of using req and res outside of function
+     */
+
+
+    searchAndRender(req, res);
 });
+
+function searchAndRender(req, res) {
+    https.get(options, function(result) {
+        let chunks = [];
+        // Datachunks sent back is incrementally pushed into an array.
+        result.on('data', (d) => {
+
+            //d == error, no information comes through. does options need an update?
+            chunks.push(d);
+        });
+
+        // Piece together chunks in array and parse.
+
+        result.on('end', () => {
+            let data = Buffer.concat(chunks);
+            list = JSON.parse(data);
+            res.render('recipes', {
+                title: 'K-Planleggeren',
+                search: search,
+                data: list});
+
+        });
+    });
+}
+
 
 //Parameters for API search.
 
@@ -48,12 +88,10 @@ function onSearch() {
         }
     };
 
-    https.get(options, function(res) {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
+    https.get(options, function(result) {
         let chunks = [];
         // Datachunks sent back is incrementally pushed into an array.
-        res.on('data', (d) => {
+        result.on('data', (d) => {
 
             //d == error, no information comes through. does options need an update?
             chunks.push(d);
@@ -61,12 +99,16 @@ function onSearch() {
 
         // Piece together chunks in array and parse.
 
-        res.on('end', () => {
+        result.on('end', () => {
             let data = Buffer.concat(chunks);
             list = JSON.parse(data);
+            res.render('recipes', {
+                title: 'K-Planleggeren',
+                search: search,
+                data: list});
 
         });
-    }).end;
+    });
 
 }
 
@@ -91,19 +133,17 @@ let options = {
 
 */
 
-var req = https.get(options, (res) => {
-    //    console.log('statusCode:', res.statusCode);
-    //    console.log('headers:', res.headers);
+var req = https.get(options, (resu) => {
     let chunks = [];
 
     // Datachunks sent back is incrementally pushed into an array.
-    res.on('data', (d) => {
+    resu.on('data', (d) => {
         chunks.push(d);
     });
 
     // Piece together chunks in array and parse.
 
-    res.on('end', () => {
+    resu.on('end', () => {
         let data = Buffer.concat(chunks);
         list = JSON.parse(data);
     });
