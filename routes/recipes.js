b4a967c03269f6e22a95de41e354ba6cd568d441
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-12-13 12:52:21 +0100

    Removed errors from recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,7 +5,7 @@ let config = require('../config');
 
 
 
-
+/*
 //Have in own file, get by json "recipe", "search", etc
 let options = {
     host: 'kolonial.no',
@@ -19,88 +19,88 @@ let options = {
         "X-Client-Token": config.secrettoken
     }
 };
-
-/* GET recipes page. */
-router.get('/', function(req, res) {
-    let searchWord = "";
-    let resultList = "";
-
-    res.render('recipes', {
-        title: 'K-Planleggeren',
-        search: searchWord,
-        data: resultList});
-});
-
-router.post('/', function(req, res){
-    let searchWord = req.body.formsearch;
-
-    console.log("Formsearch: " + searchWord);
-    updateResultListBasedOnSearch(req,res, searchWord);
-    /***
-     * Start of testing of using req and res outside of function
-     */
-});
-
-function updatePath(searchWord) {
-    options.path = '/api/v1/search/recipes/?q=' + searchWord;
-}
-
-function updateResultListBasedOnSearch(req, res, searchWord) {
-    updatePath(searchWord);
-
-    https.get(options, function(result) {
-        let chunks = [];
-        // Datachunks sent back is incrementally pushed into an array.
-        result.on('data', (d) => {
-
-            //d == error, no information comes through. does options need an update?
-            chunks.push(d);
-        });
-
-        // Piece together chunks in array and parse.
-
-        result.on('end', () => {
-            let data = Buffer.concat(chunks);
-            let resultList = JSON.parse(data);
-            res.render('recipes', {
-                title: 'K-Planleggeren',
-                search: searchWord,
-                data: resultList});
-
-        });
-    });
-}
-/*
-
-  API request to kolonial.no
-  This should be a module
-
-*/
-
-var req = https.get(options, (res) => {
-    let chunks = [];
-
-    // Datachunks sent back is incrementally pushed into an array.
-    res.on('data', (d) => {
-        chunks.push(d);
-    });
-
-    // Piece together chunks in array and parse.
-
-    res.on('end', () => {
-        let data = Buffer.concat(chunks);
-        let resultList = JSON.parse(data);
-        res.render('recipes', {
-            title: 'K-Planleggeren',
-            search: searchWord,
-            data: resultList});
-    });
-});
-
-req.on('error', (e) => {
-    console.error(e);
-});
-
-req.end();
-
+//
+// /* GET recipes page. */
+// router.get('/', function(req, res) {
+//     let searchWord = "";
+//     let resultList = "";
+//
+//     res.render('recipes', {
+//         title: 'K-Planleggeren',
+//         search: searchWord,
+//         data: resultList});
+// });
+//
+// router.post('/', function(req, res){
+//     let searchWord = req.body.formsearch;
+//
+//     console.log("Formsearch: " + searchWord);
+//     updateResultListBasedOnSearch(req,res, searchWord);
+//     /***
+//      * Start of testing of using req and res outside of function
+//      */
+// });
+//
+// function updatePath(searchWord) {
+//     options.path = '/api/v1/search/recipes/?q=' + searchWord;
+// }
+//
+// function updateResultListBasedOnSearch(req, res, searchWord) {
+//     updatePath(searchWord);
+//
+//     https.get(options, function(result) {
+//         let chunks = [];
+//         // Datachunks sent back is incrementally pushed into an array.
+//         result.on('data', (d) => {
+//
+//             //d == error, no information comes through. does options need an update?
+//             chunks.push(d);
+//         });
+//
+//         // Piece together chunks in array and parse.
+//
+//         result.on('end', () => {
+//             let data = Buffer.concat(chunks);
+//             let resultList = JSON.parse(data);
+//             res.render('recipes', {
+//                 title: 'K-Planleggeren',
+//                 search: searchWord,
+//                 data: resultList});
+//
+//         });
+//     });
+// }
+// /*
+//
+//   API request to kolonial.no
+//   This should be a module
+//
+// */
+//
+// var req = https.get(options, (res) => {
+//     let chunks = [];
+//
+//     // Datachunks sent back is incrementally pushed into an array.
+//     res.on('data', (d) => {
+//         chunks.push(d);
+//     });
+//
+//     // Piece together chunks in array and parse.
+//
+//     res.on('end', () => {
+//         let data = Buffer.concat(chunks);
+//         resultList = JSON.parse(data);
+//         let resultList = JSON.parse(data);
+//         res.render('recipes', {
+//             title: 'K-Planleggeren',
+//             search: searchWord,
+//             data: resultList});
+//     });
+// });
+//
+// req.on('error', (e) => {
+//     console.error(e);
+// });
+//
+// req.end();
 module.exports = router;
