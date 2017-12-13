5020d6994b8fd04cfa65bb1de30320bbdec65c78
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-12-13 13:31:50 +0100

    Fixed recipes according to new API wrapper

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,106 +1,32 @@
-let express = require('express');
-let router = express.Router();
-let https = require('https');
-let config = require('../config');
+const express = require('express');
+const router = express.Router();
+const connection = require('../components/kolonialapi/connection');
 
 
+/* GET search page. */
+router.get('/', function(req, res) {
+    let search = "";
+    let list = [];
 
-/*
-//Have in own file, get by json "recipe", "search", etc
-let options = {
-    host: 'kolonial.no',
-    port: 443,
-    path: '/api/v1/search/recipes/?q=',
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        "User-Agent": config.secretusername,
-        "X-Client-Token": config.secrettoken
-    }
-};
-//
-// /* GET recipes page. */
-// router.get('/', function(req, res) {
-//     let searchWord = "";
-//     let resultList = "";
-//
-//     res.render('recipes', {
-//         title: 'K-Planleggeren',
-//         search: searchWord,
-//         data: resultList});
-// });
-//
-// router.post('/', function(req, res){
-//     let searchWord = req.body.formsearch;
-//
-//     console.log("Formsearch: " + searchWord);
-//     updateResultListBasedOnSearch(req,res, searchWord);
-//     /***
-//      * Start of testing of using req and res outside of function
-//      */
-// });
-//
-// function updatePath(searchWord) {
-//     options.path = '/api/v1/search/recipes/?q=' + searchWord;
-// }
-//
-// function updateResultListBasedOnSearch(req, res, searchWord) {
-//     updatePath(searchWord);
-//
-//     https.get(options, function(result) {
-//         let chunks = [];
-//         // Datachunks sent back is incrementally pushed into an array.
-//         result.on('data', (d) => {
-//
-//             //d == error, no information comes through. does options need an update?
-//             chunks.push(d);
-//         });
-//
-//         // Piece together chunks in array and parse.
-//
-//         result.on('end', () => {
-//             let data = Buffer.concat(chunks);
-//             let resultList = JSON.parse(data);
-//             res.render('recipes', {
-//                 title: 'K-Planleggeren',
-//                 search: searchWord,
-//                 data: resultList});
-//
-//         });
-//     });
-// }
-// /*
-//
-//   API request to kolonial.no
-//   This should be a module
-//
-// */
-//
-// var req = https.get(options, (res) => {
-//     let chunks = [];
-//
-//     // Datachunks sent back is incrementally pushed into an array.
-//     res.on('data', (d) => {
-//         chunks.push(d);
-//     });
-//
-//     // Piece together chunks in array and parse.
-//
-//     res.on('end', () => {
-//         let data = Buffer.concat(chunks);
-//         resultList = JSON.parse(data);
-//         let resultList = JSON.parse(data);
-//         res.render('recipes', {
-//             title: 'K-Planleggeren',
-//             search: searchWord,
-//             data: resultList});
-//     });
-// });
-//
-// req.on('error', (e) => {
-//     console.error(e);
-// });
-//
-// req.end();
+    res.render('recipes', {
+        title: 'K-Planleggeren',
+        search: search,
+        data: list});
+
+});
+
+
+router.post('/', function(req, res){
+    let search = req.body.formsearch;
+
+    connection.kolonialRecipes(search, function(list){
+
+        res.render('recipes', {
+            title: 'K-Planleggeren',
+            search: search,
+            data: list});
+
+    })
+});
+
 module.exports = router;
