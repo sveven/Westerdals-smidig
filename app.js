9cc6aecc94cc7d7186368b89b1d3e83ef5d1f44b
henriktorget <henriktorget@gmail.com>
2017-12-15 10:57:49 +0100

    Commentet unfinished routes

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ const users = require('./routes/users');
 const search = require('./routes/search');
 const recipes = require('./routes/recipes');
 const authenticate = require('./routes/authenticate');
+/*
 const basicProducts = require('./routes/basic-products');
 const introduction = require('./routes/introduction');
 const shoppingCart = require('./routes/shopping-cart');
 const weekPlannerCurrent = require('./routes/week-planner-current');
-
+*/
 
 
 const app = express();
@@ -41,11 +42,12 @@ app.use('/search', search);
 app.use('/users', users);
 app.use('/recipes', recipes);
 app.use('/authenticate', authenticate);
+/*
 app.use('/basic-products', basicProducts);
 app.use('/introduction', introduction);
 app.use('/shopping-cart', shoppingCart);
 app.use('/week-planner', weekPlannerCurrent);
-
+*/
 reload(app);
 
 // catch 404 and forward to error handler
