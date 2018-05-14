e0b7d032e0fb59a492e4d670024574a91d612942
sveven <even.romo@gmail.com>
2018-05-14 10:15:16 +0200

    Added ejs-locals

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 //SET DEBUG=kolonial:* & npm start
 
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const reload = require('reload');
-const session = require('express-session');
+const express = require("express");
+const engine = require("ejs-locals");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const reload = require("reload");
+const session = require("express-session");
 
-const authentication = require('./components/config/authentication');
+const authentication = require("./components/config/authentication");
 //var config = require('./config');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
-const search = require('./routes/search');
-const recipes = require('./routes/recipes');
-const authenticate = require('./routes/authenticate');
-const login = require('./routes/login')
-const logout = require('./routes/logout');
-const test = require('./routes/test');
-const weekPlannerCurrent = require('./routes/week-planner-current');
+const index = require("./routes/index");
+const users = require("./routes/users");
+const search = require("./routes/search");
+const recipes = require("./routes/recipes");
+const authenticate = require("./routes/authenticate");
+const login = require("./routes/login");
+const logout = require("./routes/logout");
+const test = require("./routes/test");
+const weekPlannerCurrent = require("./routes/week-planner-current");
 
 /*
 const basicProducts = require('./routes/basic-products');
@@ -31,53 +32,56 @@ const shoppingCart = require('./routes/shopping-cart');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //app.use(session({secret: 'kolonial', saveUninitialized: true, resave: false}))
 
 app.use(authentication);
 
-app.use('/', index);
-app.use('/search', search);
-app.use('/users', users);
-app.use('/recipes', recipes);
-app.use('/authenticate', authenticate);
-app.use('/test', test);
-app.use('/login', login);
-app.use('/logout', logout);
+app.engine("engine", engine);
+app.use("view engine", "ejs");
+
+app.use("/", index);
+app.use("/search", search);
+app.use("/users", users);
+app.use("/recipes", recipes);
+app.use("/authenticate", authenticate);
+app.use("/test", test);
+app.use("/login", login);
+app.use("/logout", logout);
 /*
 app.use('/basic-products', basicProducts);
 app.use('/introduction', introduction);
 app.use('/shopping-cart', shoppingCart);
 */
-app.use('/week-planner-current', weekPlannerCurrent);
+app.use("/week-planner-current", weekPlannerCurrent);
 
 reload(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
