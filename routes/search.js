e01fafb014e9e0b074873e8b60eceae3bd9da4b0
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-05-15 11:01:31 +0200

    Removed redundancy from search

--- routes/search.js
+++ routes/search.js
@@ -40,63 +40,6 @@ router.post("/", function(req, res){
 
 		}
 	});
-
-
-/*
-    let options = {
-        host: 'kolonial.no',
-        port: 443,
-        path: '/api/v1/search/?q=' + search,
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "User-Agent": config.secretusername,
-            "X-Client-Token": config.secrettoken
-        }
-    };
-*/
-
-	/*
-      API
-    */
-/*
-    const apireq = https.get(options, (apires) =>{
-        //console.log('statusCode', apires.statusCode);
-        //console.log('headers:', apires.headers);
-        //console.log("API:" + search);
-
-        let chunks = [];
-
-// Datachunks sent back is incrementally pushed into an array.
-        apires.on('data', (d) => {
-            chunks.push(d);
-        });
-// Piece together chunks in array and parse.
-
-  apires.on('end', () => {
-    let data = Buffer.concat(chunks);
-    list = JSON.parse(data);
-    //console.log(list);
-
-    //res.send(data);
-
-    res.render('search', {
-      title: 'K-Planleggeren',
-      search: req.body.formsearch,
-      data: list});
-
-  });
-});
-
-apireq.on('error', (e) => {
-  console.error(e);
-});
-
-apireq.end();
-*/
-
-
 });
 
 
