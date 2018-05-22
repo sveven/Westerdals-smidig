22a45a64f01fc23abdb4a7aeb0e2c78868554637
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-05-22 15:39:43 +0200

    Finished queries regarding creation of meal with adding of products

--- models/productforuser.js
+++ models/productforuser.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
   const ProductForUser = sequelize.define("ProductForUser", {
-    productQuantity: DataTypes.FLOAT
+    productQuantity: DataTypes.FLOAT(15, 3)
   });
 
   return ProductForUser;
--- routes/querytest.js
+++ routes/querytest.js
@@ -14,7 +14,7 @@ router.post("/", function(req, res) {
   let userInput = req.body.formfield;
   let fetchData = "";
   
-  query.addMealToDayQuery("Monday", userInput, "Dinner", 4, 1, 1);
+  query.addMealToDayQuery(userInput, "Dinner", 4, 1);
 
   // query.createProductQuery(userInput);
   
@@ -22,7 +22,7 @@ router.post("/", function(req, res) {
   //   fetchData = JSON.stringify(fetchResponse);
   // });
   
-  console.log("FETCHDATA: " + fetchData);
+  // console.log("FETCHDATA: " + fetchData);
 
   res.render("querytest", {
     title: "K-Planleggeren",
