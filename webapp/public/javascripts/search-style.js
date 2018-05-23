48f6590f831dd373211cf2cb19458b60376da552
AnwarGit <hauale16@student.westerdals.no>
2018-05-23 11:56:09 +0200

    Added fadeIn to search. (jquery)

---
+++ webapp/public/javascripts/search-style.js
@@ -0,0 +1,20 @@
+//Variables
+
+//HTML objects
+
+let searchResult;
+let searchBtn;
+
+const init = function () {
+	const setElements = function () {
+		searchResult = $(".search-result");
+		searchBtn = $("#grocery-search-btn");
+	}();
+
+	const setEvents = function () {
+		searchBtn.click(function () {
+			searchResult.toggle();
+			searchResult.fadeIn(1200);
+		});
+	}();
+}();
