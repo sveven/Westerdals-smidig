74309e696f52d43e0c6e8fa9edcb3b087dc43914
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-06-04 10:02:00 +0200

    Added database-query route

---
+++ webapp/routes/database.js
@@ -0,0 +1,19 @@
+//Legge til vare til dag
+//Fjerne vare knyttet til dag
+
+//Legge til vare til uke
+//Fjerne vare knyttet til uke
+
+//Legge til vare til måltid
+//Fjerne vare knyttet til måltid
+
+//Legg til oppskrift
+//Fjerne oppskrift
+
+//Hente ut alle varer i én uke
+
+//Hente ut alle måltider til en dag
+
+//Hente ut varer koblet til en type i en dag
+
+//Hent ut all informasjon til handlekurven
