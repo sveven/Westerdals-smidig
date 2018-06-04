175dd6ef449db31beeebb411c12cdce6adfd95d2
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-06-04 09:36:10 +0200

    Added a test config file.

--- webapp/public/javascripts/week-planner.js
+++ webapp/public/javascripts/week-planner.js
@@ -1,11 +1,7 @@
-import create from ("../../queries/plannerCreateQueries.js");
 (function ($) {
     
     $(".dinner-wrapper, .lunch-wrapper, .breakfast-wrapper").on("click", "div", function () {
         let type = this.id.substring(0, this.id.indexOf("-"));
         let day = this.id.substring(this.id.indexOf("-") + 1, this.id.length);
-        console.log("Cookies" + JSON.stringify(Cookies.getJSON("planleggerId")));
     });
-
-
 })(jQuery);
