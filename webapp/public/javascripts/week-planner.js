31bcce2d5e7cb71dd1eb8b1a196b715341718251
AnwarGit <hauale16@student.westerdals.no>
2018-06-05 16:01:02 +0200

    Added breadcrumbs to searchPartial

--- webapp/public/javascripts/week-planner.js
+++ webapp/public/javascripts/week-planner.js
@@ -1,39 +1,38 @@
 (function ($) {
-    //variables
-    let getClickedID;
+	//variables
+	let getClickedID;
 
-    $(".dinner-wrapper, .lunch-wrapper, .breakfast-wrapper").on("click", "div", function () {
-        getClickedID = this.id;
-        let type = this.id.substring(0, this.id.indexOf("-"));
-        let day = this.id.substring(this.id.indexOf("-") + 1, this.id.length);
-    });
+	$(".dinner-wrapper, .lunch-wrapper, .breakfast-wrapper").on("click", "div", function () {
+		getClickedID = this.id;
+		let type = this.id.substring(0, this.id.indexOf("-"));
+		let day = this.id.substring(this.id.indexOf("-") + 1, this.id.length);
+	});
 
-// {"Breakfast-Monday":
-// {"Meals":[{"Title":1,
-// "Image":"imageurl",
-// "recipeId":2456}],
-// "Products":[{"identifier": 
-// {"Title":"Navnet på varen", 
-// "Image": "Imageurl", 
-// "Price":23}}]}
-// }
+	// {"Breakfast-Monday":
+	// {"Meals":[{"Title":1,
+	// "Image":"imageurl",
+	// "recipeId":2456}],
+	// "Products":[{"identifier": 
+	// {"Title":"Navnet på varen", 
+	// "Image": "Imageurl", 
+	// "Price":23}}]}
+	// }
 
-    function appendMealToPlanner(){
-        if(getClickedID ==) {
-            let mealUL = $("<ul class='meal-ul'>")
-            let mealTitle = $("<p class='meal-title'>" + Meals[i].Title + "</p>");
-            let mealImage = $("<img class='meal-img img-thumb img-thumbnail img-fluid' src='" + Meals[i].Image + "'" + "/>");
-
-        }
-    }
-
-    function appendItemToPlanner(){
-        if(getClickedID ==) {
-            let groceryUL = $("<ul class='grocery-ul'>")
-            let groceryTitle = $("<p class='grocery-title'>" + Products[i].identifier.Title + "</p>");
-            let groceryImage = $("<img class='meal-img img-thumb img-thumbnail img-fluid' src='" + Products[i].identifier.Image + "'" + "/>");
-            let groceryPrice = $("<p class='grocery-price'>" + "Kr " + Products[i].identifier.Price + ",-" + "</p>");
-        }
-    }
+	function appendMealToPlanner() {
+		let checkData = inputData;
+		if (getClickedID == Object.keys(checkData)) {
+			for (i in Meals[i]) {
+				let mealUL = $("<ul class='meal-ul'>");
+				let mealTitle = $("<p class='meal-title'>" + Meals[i].Title + "</p>");
+				let mealImage = $("<img class='meal-img img-thumb img-thumbnail img-fluid' src='" + Meals[i].Image + "'" + "/>");
+			}
+			for (i in Products[i].indentifier) {
+				let groceryUL = $("<ul class='grocery-ul'>")
+				let groceryTitle = $("<p class='grocery-title'>" + Products[i].identifier.Title + "</p>");
+				let groceryImage = $("<img class='meal-img img-thumb img-thumbnail img-fluid' src='" + Products[i].identifier.Image + "'" + "/>");
+				let groceryPrice = $("<p class='grocery-price'>" + "Kr " + Products[i].identifier.Price + ",-" + "</p>");
+			}
+		}
+	}
 
 })(jQuery);
