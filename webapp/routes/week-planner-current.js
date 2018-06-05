9de014f48030eba04495787956416d48bcd50aa8
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-06-05 23:33:37 +0200

    Added a fix for getting out JSON

--- webapp/routes/week-planner-current.js
+++ webapp/routes/week-planner-current.js
@@ -10,34 +10,52 @@ router.get("/", function(req, res) {
   let search = "";
   let list = [];
 
-  getAllIdsFromWeek(req).then(idArrays => {
-    let recipes = idArrays[0].map(recipeId => {
-      return helper.getInformationFromRecipe(recipeId[0]).then(res => {
-		  //TODO: create object here
-		  console.log(res);
-		  
-		  let recipe = {
-			  Title: res.title,
-			  Image: res.image_url,
-			  recipeId: res.id
-		  };
-        return "Recipe " + recipe;
-      });
-    });
-
-    let products = idArrays[1].map(productId => {
-      return helper.getInformationOfProduct(productId[0]).then(res => {
-		  //TODO: create object here
-		  return "Product " + res.id;
-      });
-    });
-
-    Promise.all([].concat.apply(products, recipes)).then(res => {
-		
-
-      console.log("res " + res);
-    });
+  getAllInfo(req).then(result => {
+    console.log(result);
   });
+  // getDaysInCurrentWeek(req).then(days => {
+  // 	return days.map(day => {
+  // 		let identifier = day.type + "-" + day.day;
+
+  // 		return Promise.all(
+  // 			[].concat.apply(
+  // 				getAllProductInformationForDay(day),
+  // 				getAllRecipeInformationForDay(day)
+  // 			)
+  // 		).then(res => {
+  // 			let dayObject = {};
+  // 			dayObject[identifier] = res;
+
+  // 			return dayObject;
+  // 		});
+  // 	});
+  // });
+
+  // getAllIdsFromWeek(req).then(idArrays => {
+  // 	let recipes = idArrays[0].map(recipeId => {
+  // 		return helper.getInformationFromRecipe(recipeId[0]).then(res => {
+  // 			//TODO: create object here
+
+  // 			let recipe = {
+  // 				Title: res.title,
+  // 				Image: res.image_url,
+  // 				recipeId: res.id
+  // 			};
+  // 			return "Recipe " + recipe;
+  // 		});
+  // 	});
+
+  // 	let products = idArrays[1].map(productId => {
+  // 		return helper.getInformationOfProduct(productId[0]).then(res => {
+  // 			//TODO: create object here
+  // 			return "Product " + res.id;
+  // 		});
+  // 	});
+
+  // 	Promise.all([].concat.apply(products, recipes)).then(res => {
+
+  // 	});
+  // });
 
   res.render("week-planner-current", {
     title: "K-Planleggeren",
@@ -46,20 +64,53 @@ router.get("/", function(req, res) {
   });
 });
 
-function getAllIdsFromWeek(req) {
-  let recipeIds = [];
-  let productIds = [];
-  return getDaysInCurrentWeek(req).then(days => {
-    days.forEach(day => {
-      if (getRecipeIdsInDay(day).length !== 0) {
-        recipeIds.push(getRecipeIdsInDay(day));
-      }
+function getAllInfo(req) {
+  //   let recipeIds = [];
+  //   let productIds = [];
 
-      if (getProductIdsInDay(day).length !== 0) {
-        productIds.push(getProductIdsInDay(day));
-      }
+  return getDaysInCurrentWeek(req).then(days => {
+    return Promise.all(getAllDaysInfo(days))
+  });
+
+  // return days.map(day => {
+  //   let identifier = day.type + "-" + day.day;
+
+  //   return Promise.all(
+  //     [].concat.apply(
+  //       getAllProductInformationForDay(day),
+  //       getAllRecipeInformationForDay(day)
+  //     )
+  //   ).then(res => {
+  //     let dayObject = {};
+  //     dayObject[identifier] = res;
+  //     return dayObject;
+  //   });
+
+  // if (getRecipeIdsInDay(day).length !== 0) {
+  //     recipeIds.push(getRecipeIdsInDay(day));
+  //   }
+
+  //   if (getProductIdsInDay(day).length !== 0) {
+  //     productIds.push(getProductIdsInDay(day));
+  //   }
+  // });
+  // return [recipeIds, productIds];
+}
+
+function getAllDaysInfo(days) {
+  return days.map(day => {
+    let identifier = day.type + "-" + day.day;
+
+    return Promise.all(
+      [].concat.apply(
+        getAllProductInformationForDay(day),
+        getAllRecipeInformationForDay(day)
+      )
+    ).then(res => {
+      let dayObject = {};
+      dayObject[identifier] = res;
+      return dayObject;
     });
-    return [recipeIds, productIds];
   });
 }
 
@@ -75,6 +126,33 @@ function getDaysInCurrentWeek(req) {
 }
 
 function getAllInformationForDay(day) {}
+
+/**
+ * This is a promise which will be put in a promise.all()
+ */
+function getAllRecipeInformationForDay(day) {
+  return day.Meals.map(meal => {
+    return helper.getInformationFromRecipe(meal.recipeId).then(res => {
+      return {
+        Title: res.title,
+        Image: res.image_url,
+        RecipeId: res.id
+      };
+    });
+  });
+}
+
+function getAllProductInformationForDay(day) {
+  return day.Products.map(product => {
+    return helper.getInformationOfProduct(product.kolonialId).then(res => {
+      return {
+        Name: res.full_name,
+        Image: res.images[0].thumbnail.url,
+        Price: res.gross_price
+      };
+    });
+  });
+}
 
 function getRecipeIdsInDay(day) {
   let recipeIds = [];
