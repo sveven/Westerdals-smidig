498a4ed452d3cac85d449b875b67781ea33f0727
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-06-06 09:42:40 +0200

    Cleaned up code in week-planner. Fixed naming in route on databases.

--- webapp/routes/week-planner-current.js
+++ webapp/routes/week-planner-current.js
@@ -10,52 +10,11 @@ router.get("/", function(req, res) {
   let search = "";
   let list = [];
 
-  getAllInfo(req).then(result => {
-    console.log(result);
-  });
-  // getDaysInCurrentWeek(req).then(days => {
-  // 	return days.map(day => {
-  // 		let identifier = day.type + "-" + day.day;
-
-  // 		return Promise.all(
-  // 			[].concat.apply(
-  // 				getAllProductInformationForDay(day),
-  // 				getAllRecipeInformationForDay(day)
-  // 			)
-  // 		).then(res => {
-  // 			let dayObject = {};
-  // 			dayObject[identifier] = res;
-
-  // 			return dayObject;
-  // 		});
-  // 	});
-  // });
-
-  // getAllIdsFromWeek(req).then(idArrays => {
-  // 	let recipes = idArrays[0].map(recipeId => {
-  // 		return helper.getInformationFromRecipe(recipeId[0]).then(res => {
-  // 			//TODO: create object here
-
-  // 			let recipe = {
-  // 				Title: res.title,
-  // 				Image: res.image_url,
-  // 				recipeId: res.id
-  // 			};
-  // 			return "Recipe " + recipe;
-  // 		});
-  // 	});
-
-  // 	let products = idArrays[1].map(productId => {
-  // 		return helper.getInformationOfProduct(productId[0]).then(res => {
-  // 			//TODO: create object here
-  // 			return "Product " + res.id;
-  // 		});
-  // 	});
-
-  // 	Promise.all([].concat.apply(products, recipes)).then(res => {
-
-  // 	});
-  // });
+  //TODO: Continue working with this json file.
+  getAllInformationAsJson(req).then(res => {
+	  console.log();
+	  
+  })
 
   res.render("week-planner-current", {
     title: "K-Planleggeren",
@@ -64,40 +23,24 @@ router.get("/", function(req, res) {
   });
 });
 
-function getAllInfo(req) {
-  //   let recipeIds = [];
-  //   let productIds = [];
-
+function getAllInformationAsJson(req) {
   return getDaysInCurrentWeek(req).then(days => {
-    return Promise.all(getAllDaysInfo(days))
+    return Promise.all(getInformationForAllDays(days));
   });
-
-  // return days.map(day => {
-  //   let identifier = day.type + "-" + day.day;
-
-  //   return Promise.all(
-  //     [].concat.apply(
-  //       getAllProductInformationForDay(day),
-  //       getAllRecipeInformationForDay(day)
-  //     )
-  //   ).then(res => {
-  //     let dayObject = {};
-  //     dayObject[identifier] = res;
-  //     return dayObject;
-  //   });
-
-  // if (getRecipeIdsInDay(day).length !== 0) {
-  //     recipeIds.push(getRecipeIdsInDay(day));
-  //   }
-
-  //   if (getProductIdsInDay(day).length !== 0) {
-  //     productIds.push(getProductIdsInDay(day));
-  //   }
-  // });
-  // return [recipeIds, productIds];
 }
 
-function getAllDaysInfo(days) {
+function getDaysInCurrentWeek(req) {
+  return fetch
+    .fetchAllProductsInWeek(req.cookies.ukeId)
+    .then(result => {
+      return result.Days;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function getInformationForAllDays(days) {
   return days.map(day => {
     let identifier = day.type + "-" + day.day;
 
@@ -114,18 +57,6 @@ function getAllDaysInfo(days) {
   });
 }
 
-function getDaysInCurrentWeek(req) {
-  return fetch
-    .fetchAllProductsInWeek(req.cookies.ukeId)
-    .then(result => {
-      return result.Days;
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}
-
-function getAllInformationForDay(day) {}
 
 /**
  * This is a promise which will be put in a promise.all()
@@ -152,25 +83,6 @@ function getAllProductInformationForDay(day) {
       };
     });
   });
-}
-
-function getRecipeIdsInDay(day) {
-  let recipeIds = [];
-  day.Meals.forEach(meal => {
-    recipeIds.push(meal.recipeId);
-  });
-
-  return recipeIds;
-}
-
-function getProductIdsInDay(day) {
-  let productIds = [];
-
-  day.Products.forEach(product => {
-    productIds.push(product.kolonialId);
-  });
-
-  return productIds;
 }
 
 module.exports = router;
