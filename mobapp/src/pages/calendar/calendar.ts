7827cdf220bc36f3f79aa02bad958a2a6fb2ee2f
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-06-10 23:45:28 +0200

    Added database-query to calendar page, missing route to be pushed to server.

--- mobapp/src/pages/calendar/calendar.ts
+++ mobapp/src/pages/calendar/calendar.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { DatabaseProvider } from "../../providers/database/database";
+import { Storage } from "@ionic/storage";
 
 @IonicPage()
 @Component({
-  selector: 'page-calendar',
-  templateUrl: 'calendar.html',
+  selector: "page-calendar",
+  templateUrl: "calendar.html"
 })
 export class CalendarPage {
+  weeks = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private storage: Storage,
+    private databaseProvider: DatabaseProvider
+  ) {
+    this.getAllWeeks();
+    console.log(this.weeks);
+    
   }
 
- 
-
+  getAllWeeks() {
+    this.storage.get("kolonialUserId").then(res => {
+      this.databaseProvider
+        .getAllWeeksFromServer(res)
+        .then((plannerUsers: any) => {
+          console.log("users", plannerUsers);
+          this.weeks = [];
+          for (let user of plannerUsers) {
+            for (let week of user.Weeks) {
+              let newWeek = {
+                id: week.id,
+                name: week.name
+              };
+              if (name === null) {
+                newWeek.name = "Hektisk Uke";
+              }
+              this.weeks.push(newWeek);
+            }
+          }
+        });
+    });
+  }
 }
