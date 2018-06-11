32e896ad0b2ce33decbad879ec3490c4d8d25d9b
sveven <even.romo@gmail.com>
2018-06-11 10:12:56 +0200

    modal

--- webapp/public/javascripts/plannerModal.js
+++ webapp/public/javascripts/plannerModal.js
@@ -57,7 +57,7 @@ const plannerModal = (function() {
 				"div div div #modal-close-btn",
 				function() {
 					/* $("#myModal").modal("close"); */
-					$("#myModal").remove()
+					$("#myModal").remove();
 					$(".modal-backdrop").remove();
 				}
 			);
